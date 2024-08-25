baed9335762c977ea15d27ef014825b886abb88e
Hilda-Raymundo <hraymundod@miumg.edu.gt>
2024-08-25 15:23:57 -0600

    Se cargó el link al diseño descendente del sistema

---
+++ Diseño descendente.java
@@ -0,0 +1,5 @@
+//     Hilda Aileen Raymundo Díaz :)
+
+//     Link para ver el diseño descendente del sistema Gestión de Inventarios
+
+//     https://drive.google.com/file/d/199Wz48ISrwWwQjfqJVB9zVzbhIUDGA7Z/view?usp=sharing
