9745dc7e647625d30a95d8ca3bd07ab6738c7cf6
Hilda-Raymundo <hraymundod@miumg.edu.gt>
2024-09-09 15:39:36 -0600

    Se añadieron los metodos para manipulacion de archivos de texto

--- Login.java
+++ Login.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.*;
 
 /**
 * Esta clase contiene el login para roles de usuario del sistema de gestion de inventarios
@@ -8,28 +9,49 @@ import java.util.Scanner;
 
 public class Login{
 
+	//Inicio del método principal
 	public static void main(String[] args){
 	
-		//Acá se declaran los identificadores para el sistema
+		//Se inicializa el archivo de texto
+		Login archivoTexto = new Login();
+
+		//Se llama a los metodos para la gestion de los archivos de texto
+
+		//archivoTexto.crearArchivoDeTexto();
+		//archivoTexto.escribirEnArchivosDeTexto();
+		//archivoTexto.leerArchivoDeTexto();
+		//archivoTexto.eliminarArchivoDeTexto();
+
+
+		//Esto es la declaracion de los identificadores para el sistema
 		Scanner scan = new Scanner(System.in);
 		String usuario = "";
 		String contrasenia = "";
 		int seleccion = 0;
 
-		System.out.println("SISTEMA DE GESTION DE INVENTARIOS");
+		System.out.println("		SISTEMA DE GESTION DE INVENTARIOS");
 
-		//Acá se lee el usuario y contraseña
+		//Evaluacion del llenado de los campos usuario y contraseña
 		while(usuario == "" || contrasenia == ""){
-			System.out.println(" Ingrese su nombre de usuario: ");
+			System.out.println("\n Ingrese su nombre de usuario: ");
 			usuario = scan.nextLine();
 			System.out.println(" Ingrese su contraseña: ");
 			contrasenia = scan.nextLine();
 
-			if (usuario.equals("Hilda") && contrasenia.equals("Hilda")) {
+			//Inicio del Login, evalua si el usuario y contraseña son correctos
+			if (usuario.equals("Admin") && contrasenia.equals("Hilda")) {
 
 				System.out.print("\033[H\033[2J");  
 				System.out.flush(); 
 
+				System.out.println("			Bienvenido Administrador \n");
+				System.out.println("Este es su menú de opciones:");
+				System.out.println(" 1. Informes y estadísticas ");
+				System.out.println(" 2. Añadir usuarios ");
+				System.out.println(" 3. Salir ");
+				
+
+				//Menú principal
 				System.out.println("Bienvenido al sistema \n");
 				System.out.println("Este es el menú de opciones: ");
 				System.out.println(" 1. Gestion de productos");
@@ -38,7 +60,17 @@ public class Login{
 				System.out.println(" 4. Informes y estadisticas");
 				System.out.println(" 5. Salir \n");
 				seleccion = scan.nextInt();
-							
+
+
+				//Administrador
+				//	Funcionalidades: Informes y estadisticas
+				//Vendedor
+				//	Funcionalidades: Pedidos de compra
+				//Encargado de bodega
+				//	Funcionalidades: Gestion de productos, Control de existencias
+
+						
+				//Submenús para el menú principal
 				switch(seleccion){
 
 				case 1:
@@ -53,7 +85,8 @@ public class Login{
 					System.out.println("8. Salir");
 					break;
 				case 2:
-					System.out.println(" 			MENU DE OPCIONES \n");					System.out.println("1. Registro de entrada de productos");
+					System.out.println(" 			MENU DE OPCIONES \n");					
+					System.out.println("1. Registro de entrada de productos");
 					System.out.println("2. Registro de salida de productos");
 					System.out.println("3. Movimientos de stock");
 					System.out.println("4. Salir");
@@ -80,8 +113,89 @@ public class Login{
 			}
 			else {
 				System.out.println("Usuario o contraseña incorrecta :C");
-			}
-			
+			}	
 		}
 	}
+	//Finn del método principal
+
+
+
+
+
+
+	//se llama a la clase file para pasarle un archivo
+	File archivo;
+	private void crearArchivoDeTexto(){
+				
+		archivo = new File("login.txt");
+		try{
+			if(archivo.createNewFile()){
+				System.out.println("Creacion exitosa");
+			}
+			else{
+				//System.out.println("Error al crear archivo");
+			}
+		}catch(IOException exepcion){
+			exepcion.printStackTrace(System.out);
+
+		}	
+	}
+
+	//Método para eliminar archivos
+	private void eliminarArchivoDeTexto(){
+				
+			if(archivo.delete()){
+				System.out.println("Eliminación exitosa");
+			}
+			else{
+				//System.out.println("Error al eliminar archivo");
+			}
+	}
+
+	//Metodo para escribir en archivos
+	private void escribirEnArchivosDeTexto(){
+				
+		try{
+			FileWriter escritura  = new FileWriter(archivo);
+			escritura.write("Saludos");
+			escritura.close();
+			System.out.println("Texto añadido");
+		}catch(IOException exepcion){
+			exepcion.printStackTrace(System.out);
+		}
+
+	}
+
+	//Metodo para leer en un archivo de texto
+	private void leerArchivoDeTexto(){
+				
+		String contenido;
+
+		try{
+			FileReader lectura  = new FileReader(archivo);
+			BufferedReader lector = new BufferedReader(lectura);
+			contenido = lector.readLine();
+
+			while(contenido != null){
+				System.out.println(contenido);
+				contenido = lector.readLine();
+			}
+
+			
+		}catch(IOException exepcion){
+			exepcion.printStackTrace(System.out);
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
 }
